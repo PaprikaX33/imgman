586941cb28f26c1956bc518213013e7bc5c8529d
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2025-11-29 06:46:08 +0800

    splitted the parser
    The parser is now splitted between the raw parser for the clap and the
    normalized parser structure to be returned as a configuration

--- src/args.rs
+++ src/args.rs
@@ -1,27 +1,17 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 const DEST_SUFF: &str = ".png";
 
-#[derive(Parser, Debug)]
-#[command(name = "imgman")]
-#[command(author = "Paprika")]
-#[command(version = env!("CARGO_PKG_VERSION"))]
-#[command(
-    about = "Simple program to manipulate image",
-    long_about = "An image manipulating program for experimenting with tranparency"
-)]
+#[derive(Debug)]
 pub struct Args {
-    #[arg(value_name = "SOURCE")]
-    #[arg(help = "Source Image")]
+    mode: OperationType,
     source: String,
-    #[arg(value_name = "DESTINATION", default_value_t = String::from("manip.png"))]
-    #[arg(help = "Destination Image")]
     dest: String,
 }
 
 impl Args {
     pub fn get_args() -> Result<Self, clap::error::Error> {
-        let parsed = Self::try_parse_from(wild::args())?;
+        let parsed = ParsableArgs::try_parse_from(wild::args())?;
         let mut dest = parsed.dest.trim().to_string();
         let dest = if dest.ends_with(DEST_SUFF) {
             dest
@@ -30,7 +20,8 @@ impl Args {
             dest
         };
         let source = parsed.source.trim().to_string();
-        Ok(Self { source, dest })
+        let mode = parsed.mode;
+        Ok(Self { source, dest, mode })
     }
     pub fn source(&self) -> &str {
         &self.source
@@ -38,4 +29,32 @@ impl Args {
     pub fn dest(&self) -> &str {
         &self.dest
     }
+}
+
+#[derive(Parser)]
+#[command(name = "imgman")]
+#[command(author = "Paprika")]
+#[command(version = env!("CARGO_PKG_VERSION"))]
+#[command(
+    about = "Simple program to manipulate image",
+    long_about = "An image manipulating program for experimenting with tranparency"
+)]
+pub struct ParsableArgs {
+    #[arg(long, short, value_enum)]
+    #[arg(default_value_t = OperationType::Gray)]
+    mode: OperationType,
+    #[arg(value_name = "SOURCE")]
+    #[arg(help = "Source Image")]
+    source: String,
+    #[arg(value_name = "DESTINATION", default_value_t = String::from("manip.png"))]
+    #[arg(help = "Destination Image")]
+    dest: String,
+}
+
+#[derive(Eq, PartialEq, Clone, ValueEnum, Debug)]
+pub enum OperationType {
+    Gray,
+    GrayAlp,
+    Copy,
+    AlpToGray,
 }
